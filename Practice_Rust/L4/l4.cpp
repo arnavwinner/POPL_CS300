47b173c473db742734b22dd4800b1f65f609b340
arnavwinner <gautam.arnavtokyo@gmail.com>
2024-01-15 16:51:31 +0530

    Modified l4.cpp

--- Practice_Rust/L4/l4.cpp
+++ Practice_Rust/L4/l4.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
 	int a = 7; // a = 0, even in this case Value 3 gets printed
-	if (a = 3) {
+	if (a = 3) { // it just assigns 3 as soon as you write a = 3 in the parentheses
 		cout << "Value 3" << '\n';
 	}
 	else {
