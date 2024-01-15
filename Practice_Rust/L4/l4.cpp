319a7146723c1e5dfb8294c159242e52e023cc50
arnavwinner <gautam.arnavtokyo@gmail.com>
2024-01-15 16:49:05 +0530

    Added Lecture 4

---
+++ Practice_Rust/L4/l4.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int a = 7; // a = 0, even in this case Value 3 gets printed
+	if (a = 3) {
+		cout << "Value 3" << '\n';
+	}
+	else {
+		cout << "Not 3" << '\n';
+	}
+	return 0;
+}
---
+++ Practice_Rust/L4/l4.rs
@@ -0,0 +1,9 @@
+fn main() {
+	let mut a = 7;
+	if a == 3 { // in rust you cannot write a = 3 here, so I will write a c++ code to explain
+		println!("Value 3");
+	}
+	else {
+		println!("Not 3");
+	}
+}
