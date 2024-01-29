ceea504b99544b84390aaa77767d5b2153c28b79
arnavwinner <gautam.arnavtokyo@gmail.com>
2024-01-29 22:25:42 +0530

    Added L7

---
+++ Practice_Rust/L7/main.cpp
@@ -0,0 +1,34 @@
+
+/*
+Types were being taught in the class
+
+
+Strongly Typed, Statically Typed, Dynamically Typed
+*/
+
+// author: arnavwinner
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	// cout << "Hello World" << '\n';
+}
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
